9cbb66cacca7dd04cffa8ca2d75c8427a73467d4
angeloalmanza <angeloalmanza26@gmail.com>
2025-01-22 15:43:07 +0100

    feat: implementazione milestone 3 (funzione destroy)

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,7 +3,7 @@ const connection = require("../data/db");
 // Index
 const index = (req, res) => {
     //prelevo tutti i post
-    const sql = `SELECT * FROM posts`;
+    const sql = `SELECT * FROM posts;`;
     connection.query(sql, (err, results) => {
         //Se la query restituisce un errore, mando errore 500
         if (err) {
@@ -42,7 +42,18 @@ const show = (req, res) => {
 
 // Destroy
 const destroy = (req, res) => {
+    const id = req.params.id;
 
+    const sql = `DELETE FROM posts WHERE id = ?;`;
+    connection.query(sql, [id], (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Errore interno del server"
+            })
+        } else {
+            return res.sendStatus(204);
+        }
+    })
 }
 
 module.exports = {
