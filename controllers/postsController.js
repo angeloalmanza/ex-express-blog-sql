da442aa7137eaba89899551ce3be30f044c1947f
angeloalmanza <angeloalmanza26@gmail.com>
2025-01-22 15:51:38 +0100

    feat: implementazione milestone 4 (funzione show)

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,7 +4,7 @@ const connection = require("../data/db");
 const index = (req, res) => {
     //prelevo tutti i post
     const sql = `SELECT * FROM posts;`;
-    connection.query(sql, (err, results) => {
+    connection.query(sql, (err, posts) => {
         //Se la query restituisce un errore, mando errore 500
         if (err) {
             return res.status(500).json({
@@ -14,7 +14,7 @@ const index = (req, res) => {
         } else {
             return res.status(200).json({
                 status: "success",
-                data: results
+                data: posts
             })
         }
     })
@@ -22,7 +22,25 @@ const index = (req, res) => {
 
 // Show
 const show = (req, res) => {
+    const id = req.params.id;
 
+    const sql = `SELECT * FROM posts WHERE id = ?;`;
+    connection.query(sql, [id], (err, posts) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Errore interno del server"
+            })
+        } else if (posts.length === 0) {
+            return res.status(404).json({
+                message: "Post non trovato"
+            })
+        } else {
+            return res.status(200).json({
+                status: "success",
+                data: posts[0]
+            })
+        }
+    })
 }
 
 // // Create
